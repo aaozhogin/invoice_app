8c49d588b36f230963bef4e0584571f8e416081d
Aleksandr Ozhogin <aaozhogin@gmail.com>
2026-01-05 08:49:12 +1100

    Change to calc(100vh - 100px) - middle ground for iPad/MacBook
    - iPad cuts off at 8 PM with 120px
    - MacBook is better but still not perfect
    - Using 100px to give iPad more space
    - Should show at least to 10 PM on both devices

--- app/calendar/CalendarClient.tsx
+++ app/calendar/CalendarClient.tsx
@@ -5284,7 +5284,7 @@ export default function CalendarClient() {
           display: flex;
           gap: 0;
           overflow-x: auto;
-          height: calc(100vh - 120px);
+          height: calc(100vh - 100px);
         }
 
         .cal-week-hours-column {
